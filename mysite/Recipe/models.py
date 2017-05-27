49014d3ab856a1f58592fe6325634492c5e3bd53
ivantsui <ixt3042@g.rit.edu>
2017-05-27 17:23:42 -0400

    Defined what recipes and flavorings are

--- mysite/Recipe/models.py
+++ mysite/Recipe/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+class Recipe(models.Model):
+    name = models.TextField()
+    percentage = models.PositiveIntegerField()
+    targetAmount = models.PositiveIntegerField()
+    targetNicotine = models.PositiveIntegerField()
+    vgRatio = models.PositiveIntegerField()
+    pgRatio = models.PositiveIntegerField()
+    daysToSteep = models.PositiveIntegerField()
+
+    def __str__(self):
+        return "%s" % (self.name)
+
+class Flavoring(models.Model):
+    name = models.CharField(max_length=30)
+    percentage = models.PositiveIntegerField()
+    manufacturer = models.CharField(max_length=30)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE())
+
+    def __str__(self):
+        return "%s" % (self.name)
